6cb5ff96766dc5dc8bcdee30c800f6eef22aee82
Ashwani Kansal <74704355+ashwanikansal@users.noreply.github.com>
2020-12-13 12:39:59 +0400

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ const superior = () => {
     const menuitemlink=document.querySelectorAll('.navbar .menu li a');
     const burger= document.querySelector('.burger');
     const burgerline= document.querySelectorAll('.burger div');
+
+    const backupmove=document.querySelector('.home .max-width');
+    const para=document.querySelector('.about .content .paragraph');
+
+    const skill = document.querySelector('.skills .main');
+    const photoshop=document.querySelector('.skills .content .property .skill .photoshop');
+    const illustrator=document.querySelector('.skills .content .property .skill .illustrator');
+    const canva=document.querySelector('.skills .content .property .skill .canva');
+
     window.addEventListener('scroll', ()=>{
         const y=window.scrollY;
         if(y>20){
@@ -28,6 +37,24 @@ const superior = () => {
                 lines.classList.remove('sticky');
             })
         }
+        console.log(y);//400
+        backupmove.style.backgroundColor = `rgba(10,10,10, ${0.5+y*0.004})`;
+        
+
+        if(y > para.offsetTop/1.8 ){
+            para.classList.add('showing');
+            console.log(para);
+        }
+        // else{
+        //     para.classList.remove('showing');
+        // }
+
+        if(y > skill.offsetTop/1.3){
+            skill.classList.add('showing');
+            photoshop.style.animation = `aphotoshop 3s ease 0.6s forwards`;
+            illustrator.style.animation = `aillustrator 4s ease 0.8s forwards`;
+            canva.style.animation = `acanva 3.5s ease 1s forwards`;
+        }
     });
     burger.addEventListener('click',() => {
         menu.classList.toggle('active');
@@ -49,5 +76,7 @@ const superior = () => {
 //     
 //     
 }
+
+
 
 superior();
